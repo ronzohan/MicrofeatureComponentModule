9f3a7d81928cde71a8787a33ef22bceb50feae84
Ron Daryl Magno <ron.zohan@gmail.com>
2017-12-11 00:14:58 +0800

    In

---
+++ ComponentFeature/ComponentFeature.h
@@ -0,0 +1,19 @@
+//
+//  ComponentFeature.h
+//  ComponentFeature
+//
+//  Created by Ron Daryl Magno on 12/10/17.
+//  Copyright © 2017 Ron Daryl Magno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComponentFeature.
+FOUNDATION_EXPORT double ComponentFeatureVersionNumber;
+
+//! Project version string for ComponentFeature.
+FOUNDATION_EXPORT const unsigned char ComponentFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentFeature/PublicHeader.h>
+
+
---
+++ ComponentFeature/Sample.swift
@@ -0,0 +1,19 @@
+//
+//  Sample.swift
+//  ComponentFeature
+//
+//  Created by Ron Daryl Magno on 12/11/17.
+//  Copyright © 2017 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+import CoreFeature
+
+public struct Sample {
+    var name: String 
+    
+    public init(name: String) {
+        self.name = name
+        debugPrint("fuck you")
+    }
+}
